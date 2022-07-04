fd8294117171fb7a83db8d420813eaf7fb5051b9
Josselin PREVOST <josselin.prevost-devweb@mailo.fr>
2022-07-04 05:53:54 +0200

    ajout de la fonction de sélection des fichier avec le meilleur score + sélection des molécules selon un score de probabilité

--- code4life.js
+++ code4life.js
@@ -11,18 +11,19 @@ for (let i = 0; i < projectCount; i++) {
     const d = parseInt(inputs[3]);
     const e = parseInt(inputs[4]);
 }
-// Création de classe pour stocker les infos des bots.
+// class for bots
 class BotProperties {
-    constructor(target,eta,score,storageA,storageB,storageC,storageD,storageE,expertiseA,expertiseB,expertiseC,expertiseD,expertiseE) {
-        this.target =  target; //emplacement du bot
-        this.eta = eta; // à ignorer pour cette ligue
+    constructor(target,eta,score,storageA,storageB,storageC,storageD,storageE,expertiseA,expertiseB,expertiseC,expertiseD,expertiseE,totStorageMol) {
+        this.target =  target; // bot coordinate
+        this.eta = eta; // ignore it for this league
         this.score = score; // points de vie
-        this.storageA = storageA; // nombres de molécules A transportées par le bot
+        this.storageA = storageA; // A molécules carrying by my bot
         this.storageB = storageB;  // etc...
         this.storageC = storageC;
         this.storageD = storageD;
         this.storageE = storageE;
-        this.expertiseA = expertiseA; // à ignorer pour cette ligue
+        this.totStorageMol = storageA+storageB+storageC+storageD+storageE; // how many molecules we are carrying
+        this.expertiseA = expertiseA; // ignore it for this league
         this.expertiseB = expertiseB;
         this.expertiseC = expertiseC;
         this.expertiseD = expertiseD;
@@ -30,37 +31,32 @@ class BotProperties {
     }
 }
 
-// Création de classe pour stocker les infos des samples.
+// class for samples
 class SampleProperties {
-    constructor(sampleId,carriedBy,rank,expertiseGain,health,costA,costB,costC,costD,costE) {
-        this.sampleId = sampleId; // id unique du fichier
-        this.carriedBy = carriedBy; // 0 si le fichier est porté par mon bot / 1 si par l'autre / -1 sinon
-        this.rank = rank; // à ignorer pour cette ligue
-        this.expertiseGain = expertiseGain; // à ignorer pour cette ligue
-        this.health = health; // points de vie accordés quand fichier validé
-        this.costA = costA; // nombres de molecules A nécessaire pour valider le fichier
+    constructor(sampleId,carriedBy,rank,expertiseGain,health,costA,costB,costC,costD,costE,totCostMol) {
+        this.sampleId = sampleId; // sample's id
+        this.carriedBy = carriedBy; // 0 if sample is carried by my bot / 1 if by the other one / else -1 
+        this.rank = rank; // ignore it for this league
+        this.expertiseGain = expertiseGain; // ignore it for this league
+        this.health = health; // score
+        this.costA = costA; // A molecules needed for the solution
         this.costB = costB; // etc...
         this.costC = costC;
         this.costD = costD;
         this.costE = costE;
+        this.totCostMol = costA+costB+costC+costD+costE;
     }
 }
 
-// Création des valeurs de contrôle d'actions
-let didICameDiag = false;
-let sampleAvailable;
-let didITookSample = false;
-let didICamMol = false;
-let AmIFullOfMol = false;
-let didICamLab = false;
-let didICreatedSolution = false;
+// action's controls values
+let stage = 0;
 
 // ***************************************************************
-// DEBUT DE LA GAME LOOP
+// GAME LOOP'S START
 // ***************************************************************
 
 while (true) {
-    // assigniation des propriétées des bots dans le tableau botsProperties
+    // give bots properties in the array "botsProperties"
     let botsProperties = [];
     for (let i = 0; i < 2; i++) {
         var inputs = readline().split(' ');
@@ -68,16 +64,16 @@ while (true) {
         botsProperties.push(bot);
     }
 
-    // Valeurs de contrôles du jeux
+    // Game's controls values
     var inputs = readline().split(' ');
-    const availableA = parseInt(inputs[0]); // nombres de molecules A disponibles
+    const availableA = parseInt(inputs[0]); // A molecules available in 'MOLECULE'
     const availableB = parseInt(inputs[1]); // etc...
     const availableC = parseInt(inputs[2]);
     const availableD = parseInt(inputs[3]);
     const availableE = parseInt(inputs[4]);
-    const sampleCount = parseInt(readline()); // nombre de fichiers d'échantillons en jeux
+    const sampleCount = parseInt(readline()); // number of samples in game
 
-    // assigniation des propriétées des samples dans le tableau sampleProperties
+    // give all samples properties in the array "sampleProperties"
     let sampleProperties = []; 
     for (let i = 0; i < sampleCount; i++) {
         var inputs = readline().split(' ');
@@ -131,67 +127,206 @@ while (true) {
     const takeD = 'CONNECT D';
     const takeE = 'CONNECT E';
 
-    // données à imprimer en console.error pour débug
+    // values which need to be update each turn
+    let mySample = []; // all my samples
     sampleProperties.forEach(object=>{
         if (object.carriedBy == 0) {
-            console.error('mon sample = ');
-            console.error(object);
-        } else if (object.carriedBy == 1) {
-            console.error('sample du bot adverse = ');
-            console.error(object);
+            mySample.push(object);
         }
-    })
+    });
+    let hisSample = []; // all his samples
+    sampleProperties.forEach(object=>{
+        if (object.carriedBy == 1) {
+            hisSample.push(object);
+        }
+    });
 
-    // pour 1ère phase uniquement
-    if (me.target == 'START_POS') {
-        console.log(goD);
-        didICameDiag = true;
-    // début de la boucle => prendre un sample à DIAGNOSIS
-    } else if (didICameDiag == true) {
-        if (sampleProperties[0].carriedBy == -1) {
-            sampleAvailable = sampleProperties[0].sampleId;
-        } else if (sampleProperties[0].carriedBy == 1) {
-            sampleAvailable = sampleProperties[1].sampleId;
+    // console.error for debug
+    // mySample.forEach(object=>{
+    //     console.error(object);
+    // })
+    // hisSample.forEach(object=>{
+    //         console.error("adversaire carrying =");
+    //         console.error(object);
+    // })
+    // console.error("moi =");
+    // console.error(me);
+    // console.error("lui = ");
+    // console.error(him);
+
+    // if / else for check in what case i am.
+    if (me.target == diag) {
+        if (mySample.length < 3) {
+            stage = 1;
+        } else if (mySample.length == 3) {
+            stage = 2;
         }
-        // sampleAvailable = sampleProperties[0].sampleId;
-        let result = 'CONNECT '+ sampleAvailable;
-        console.log(result);
-        didICameDiag = false;
-        didITookSample = true;
-    // puis aller à MOLECULES
-    } else if (didITookSample == true) {
-        console.log(goM);
-        didITookSample = false;
-        didICamMol = true;
-    // prendre toutes les molécules nécessaires
-    } else if (didICamMol == true) {
-        if (me.storageA < sampleProperties[0].costA) {
-            console.log(takeA);
-        } else if (me.storageB < sampleProperties[0].costB) {
-            console.log(takeB);
-        } else if (me.storageC < sampleProperties[0].costC) {
-            console.log(takeC);
-        } else if (me.storageD < sampleProperties[0].costD) {
-            console.log(takeD);
-        } else if (me.storageE < sampleProperties[0].costE) {
-            console.log(takeE);
-    // Quand c'est fini, aller à LABORATORY
-        } else {
-            AmIFullOfMol = true;
-            didICamMol = false;
-            didICamLab = true;
-            console.log(goL);
-        } 
-    // fabriquer la solution
-    } else if (didICamLab == true) {
-        let result = 'CONNECT '+ sampleAvailable;
-        console.log(result);
-        didICamLab = false;
-        didICreatedSolution = true;
-    // puis retourner à DIAGNOSIS
-    } else if (didICreatedSolution == true) {
-        console.log(goD);
-        didICreatedSolution = false;
-        didICameDiag = true;
+    } else if (me.target == mol) {
+        if (me.totStorageMol < 10) {
+            stage = 3;
+        } else if (me.totStorageMol == 10) {
+            stage = 4;
+        }  
+    } else if (me.target == lab) {
+            stage = 5;
+    } 
+
+    // give a probability score for which molecule to take
+
+    // actions for each stage
+    switch (stage) {
+        case 0 :
+            console.log(goD); // aller à DIAGNOSIS
+        break;
+        case 1 :
+            // sort samples by score
+            for (let i in sampleProperties) {
+                for (let j in mySample) {
+                    if (sampleProperties[i].sampleId == mySample[j].sampleId) {
+                        sampleProperties.splice(i,1);
+                    }
+                }
+            }
+            sampleProperties.sort((a,b)=>b.health - a.health);
+            // and take the available one
+            for (let i = 0 ; i < sampleCount ; i++) {
+                if (sampleProperties[i].carriedBy == -1) {
+                    console.log('CONNECT '+sampleProperties[i].sampleId);
+                    break;
+                }
+            }            
+        break;
+        case 2 :
+            console.log(goM); // aller à MOLECULES
+        break;
+        case 3 :
+            // Choose the best molecule to take
+            let scoreA; // initialiser les scores
+            let scoreB;
+            let scoreC;
+            let scoreD;
+            let scoreE;
+            for (let i in mySample) { // si un des samples à besoin de la molecule, attribuer 0 / -1 sinon
+                if (mySample[i].costA-me.storageA > 0){
+                    scoreA = 0;
+                    break;
+                } else {
+                    scoreA = -1;
+                }
+            }
+            for (let i in mySample) {
+                if (mySample[i].costB-me.storageB > 0){ // etc...
+                    scoreB = 0;
+                    break;
+                } else {
+                    scoreB = -1;
+                }
+            }
+            for (let i in mySample) {
+                if (mySample[i].costC-me.storageC > 0){
+                    scoreC = 0;
+                    break;
+                } else {
+                    scoreC = -1;
+                }
+            }
+            for (let i in mySample) {
+                if (mySample[i].costD-me.storageD > 0){
+                    scoreD = 0;
+                    break;
+                } else {
+                    scoreD = -1;
+                }
+            }
+            for (let i in mySample) {
+                if (mySample[i].costE-me.storageE > 0){
+                    scoreE = 0;
+                    break;
+                } else {
+                    scoreE = -1;
+                }
+            }
+            for (let i = 0 ; i < mySample.length ; i++) { // ajouter 1 au score de la molecule si le stock total moins mes besoins - celui de l'adversaire > 0
+                for (let j = 0 ; j < hisSample.length ; j++) {
+                    if (availableA - (hisSample[j].costA + mySample[i].costA) > 0 && scoreA != -1) {
+                        scoreA++;
+                    }
+                    if (availableB - (hisSample[j].costB + mySample[i].costB) > 0 && scoreB != -1) {
+                        scoreB++;
+                    }
+                    if (availableC - (hisSample[j].costC + mySample[i].costC) > 0 && scoreC != -1){
+                        scoreC++;
+                    }
+                    if (availableD - (hisSample[j].costD + mySample[j].costD) > 0 && scoreD != -1){
+                        scoreD++;
+                    }
+                    if (availableE - (hisSample[j].scoreE + mySample[i].costE) > 0 && scoreE != -1){
+                        scoreE++;
+                    }
+                }
+            }
+            let scoreTot = [scoreA,scoreB,scoreC,scoreD,scoreE]; // ranger les scores dans un tableau qu'on va trier avec la plus grande valeur en premier
+            scoreTot.sort((a,b)=>b-a);
+            let takeWhatMolecule;
+            if (scoreTot[0] == scoreA){ // attribuer une lettre à "takeWhatMolecule" en fonction de la correspondance entre le plus grand score du tableau et celui de chaque molecules
+                takeWhatMolecule = 'A';
+            } else if (scoreTot[0] == scoreB) {
+                takeWhatMolecule = 'B';
+            } else if (scoreTot[0] == scoreC) {
+                takeWhatMolecule = 'C';
+            } else if (scoreTot[0] == scoreD) {
+                takeWhatMolecule = 'D';
+            } else if (scoreTot[0] == scoreE) {
+                takeWhatMolecule = 'E';
+            }
+            switch (takeWhatMolecule) { // prendre la molecule
+                case 'A' :
+                    console.log(takeA);
+                break;
+                case 'B':
+                    console.log(takeB);
+                break;
+                case 'C' :
+                    console.log(takeC);
+                break;
+                case 'D' :
+                    console.log(takeD);
+                break;
+                case 'E' :
+                    console.log(takeE);
+                break;
+            }
+        break;
+        case 4 :
+            console.log(goL); // aller à LABORATORY
+        break;
+        case 5 :
+            // let giveWhatSolution;
+            for (let i in mySample){
+                if (me.storageA>=mySample[i].costA && me.storageB>=mySample[i].costB && me.storageC>=mySample[i].costC && me.storageD>=mySample[i].costD && me.storageE>=mySample[i].costE) {
+                    mySample[i].health+=100;
+                    console.log("CONNECT "+mySample[i].sampleId);
+                    break;
+                } else {
+                    console.log(goD);
+                    break;
+                }
+            }
+            // switch (giveWhatSolution) {
+            //     case 0 :
+            //         console.log("CONNECT "+mySample[0].sampleId);
+            //     break;
+            //     case 1 :
+            //         console.log("CONNECT "+mySample[1].sampleId);
+            //     break;
+            //     case 2 :
+            //         console.log("CONNECT "+mySample[2].sampleId);
+            //     break;
+            //     default:
+            //         stage = 0;
+            // }
+        break;
+        default:
+            console.error("erreur de stage");
     }
 }
